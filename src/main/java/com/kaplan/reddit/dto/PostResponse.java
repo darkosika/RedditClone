5da96b8bebd9678bd3f230216c374a206efaff9e
darkosika <osmnkpln03ozgurluk@gmail.com>
2021-01-30 22:31:12 +0200

    Add Mapstruct library and Create Posts APIs

--- src/main/java/com/kaplan/reddit/dto/PostResponse.java
+++ src/main/java/com/kaplan/reddit/dto/PostResponse.java
@@ -1,18 +1,17 @@
 package com.kaplan.reddit.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class SubredditDto {
+public class PostResponse {
     private Long id;
-    private String name;
+    private String postName;
+    private String url;
     private String description;
-    private Integer numberOfPosts;
-
+    private String userName;
+    private String subredditName;
 }
